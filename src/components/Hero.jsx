7ee7f8870877b2e62cd64482320bcea89cbfc8f0
SuryaAtmaja1 <suryagame12@gmail.com>
2024-03-21 15:58:48 +0700

    fix dropdown hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,9 +19,9 @@ const Hero = () => {
   ];
 
   return (
-    <div className=" min-h-screen w-full  flex justify-center items-center">
-      <div className="lg:w-4/5 w-5/6 flex flex-col-reverse lg:flex-row justify-center gap-32 items-center">
-        <div className="flex flex-col gap-9 justify-center items-center max-w-[600px]">
+    <div className=" min-h-screen w-full flex justify-center items-center">
+      <div className="lg:w-4/5 w-5/6 flex flex-col-reverse lg:flex-row justify-center gap-32 items-start">
+        <div className="flex flex-col gap-9 justify-center h-auto items-center max-w-[600px]">
           <h1 className="align-middle text-6xl text-center lg:text-left font-bold px-4 font-playFair">
             <div className="text-primary">Hi, i'm</div>
             <div className="mt-2 text-base-content text-5xl">
@@ -30,18 +30,22 @@ const Hero = () => {
           </h1>
           <div className="w-full relative">
             <div
-              onClick={() => setOpenTitle(true)}
-              className="bg-neutral w-full text-neutral-content font-manrope text-2xl py-5 px-5"
+              onClick={() => setOpenTitle(!openTitle)}
+              className={`bg-neutral w-full text-neutral-content font-manrope text-2xl py-5 px-5 cursor-pointer ${
+                openTitle ? "rounded-t-lg" : "rounded-lg"
+              }`}
             >
-              <div className="rounded-lg flex justify-between">
+              <div className="flex justify-between">
                 <p className="text-left leading-8 left-0 px-5 font-semibold">
                   {titleSelected}
                 </p>
-                <IoMdArrowDropdown className="size-9" />
+                <IoMdArrowDropdown
+                  className={`size-9 ${openTitle ? "rotate-180" : ""}`}
+                />
               </div>
             </div>
             {openTitle && (
-              <div className="bg-neutral w-full text-neutral-content top-0 rounded-lg">
+              <div className="bg-neutral w-full absolute text-neutral-content rounded-b-lg cursor-pointer ">
                 {titles?.map((title) => (
                   <li
                     className="text-left leading-8 font-semibold list-none text-2xl left-0 py-5 px-10"
